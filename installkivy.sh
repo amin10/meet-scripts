35a007b38640b4d013711c38e9f2f08f983798d4
Amin Manna <manna@mit.edu>
2014-08-10 11:23:47 +0300

    Update installkivy.sh

--- installkivy.sh
+++ installkivy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo add-apt-repository ppa:kivy-team/kivy
 sudo apt-get install pkg-config python-setuptools python-pygame python-opengl python-gst0.10 python-enchant gstreamer0.10-plugins-good python-dev build-essential libgl1-mesa-dev libgles2-mesa-dev cython
 sudo apt-get update
 sudo add-apt-repository ppa:cython-dev/master-ppa
@@ -6,3 +7,4 @@ sudo apt-get update
 sudo apt-get install cython
 sudo apt-get update
 sudo apt-get install python-kivy
+sudo apt-get update
