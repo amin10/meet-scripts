29854a0a70153e65f9a1f2b6d78526b73b15451e
MEET <meet@meet>
2014-08-10 10:55:02 +0300

    script

---
+++ installkivy.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+sudo apt-get install pkg-config python-setuptools python-pygame python-opengl python-gst0.10 python-enchant gstreamer0.10-plugins-good python-dev build-essential libgl1-mesa-dev libgles2-mesa-dev cython
+sudo apt-get update
+sudo add-apt-repository ppa:cython-dev/master-ppa
+sudo apt-get update
+sudo apt-get install cython
+sudo apt-get update
+sudo apt-get install python-kivy
